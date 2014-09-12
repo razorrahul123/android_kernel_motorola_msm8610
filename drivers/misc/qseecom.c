0737f82b5c1974913e1adaf8ccbc52fc94de2e1d
Naveen Ramaraj <nramaraj@codeaurora.org>
2014-09-12 12:55:54 -0700

    qseecom: Fix memory leak
    The driver was not freeing up client memory from ION heap
    during app unload. Also fix un-necessary logspam.
    
    Bug: 17437202
    Change-Id: Id93dc5adcc030e5d27609746b69fe8e5e3d73ea2
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1122,8 +1122,8 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 	bool found_dead_app = false;
 
 	if (!memcmp(data->client.app_name, "keymaste", strlen("keymaste"))) {
-		pr_warn("Do not unload keymaster app from tz\n");
-		return 0;
+		pr_debug("Do not unload keymaster app from tz\n");
+		goto free_app_memory;
 	}
 
 	if (data->client.app_id > 0) {
@@ -1219,6 +1219,7 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 		spin_unlock_irqrestore(&qseecom.registered_app_list_lock,
 								flags);
 	}
+free_app_memory:
 	qseecom_unmap_ion_allocated_memory(data);
 	data->released = true;
 	return ret;
