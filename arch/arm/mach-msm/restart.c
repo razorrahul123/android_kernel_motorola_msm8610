87f1434313c130098c5ad0efb16c098620cd8740
Wei Li <sirius.liwei@huawei.com>
2017-08-31 14:20:08 -0400

    Remove 'reboot edl' interface for security.
    BUG: 63100473
    
    Change-Id: If6cb5ce50524d34b30f09a22eeb76d2cd3a0e834

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -108,6 +108,7 @@ static bool get_dload_mode(void)
 	return dload_mode_enabled;
 }
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	if (emergency_dload_mode_addr) {
@@ -126,6 +127,7 @@ static void enable_emergency_dload_mode(void)
 		mb();
 	}
 }
+#endif
 
 static int dload_set(const char *val, struct kernel_param *kp)
 {
@@ -292,8 +294,10 @@ static void msm_restart_prepare(const char *cmd)
 			unsigned long code;
 			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
 			__raw_writel(0x6f656d00 | code, restart_reason);
+#if 0
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
+#endif
 		} else {
 			__raw_writel(0x77665501, restart_reason);
 		}
