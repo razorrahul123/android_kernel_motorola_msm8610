06bd111dd27fe6e1d908c0dead2e34892095bef5
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-11-22 13:00:27 +0000

    Revert " brlocks/lglocks: API cleanups"
    This reverts commit f270ebdd545d703d7b3c97673ce0f8b0e42036c3.

--- fs/dcache.c
+++ fs/dcache.c
@@ -1048,6 +1048,7 @@ void shrink_dcache_for_umount(struct super_block *sb)
 	}
 }
 
+
 /*
  * Search for at least 1 mount point in the dentry's subdirs.
  * We descend to the next level whenever the d_subdirs
@@ -2611,7 +2612,7 @@ global_root:
 	if (!slash)
 		error = prepend(buffer, buflen, "/", 1);
 	if (!error)
-		error = is_mounted(vfsmnt) ? 1 : 2;
+		error = real_mount(vfsmnt)->mnt_ns ? 1 : 2;
 	return error;
 }
 
